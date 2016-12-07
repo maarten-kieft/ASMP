c314fdf7f30de650d8f4b3b325120318505c5853
Maarten Kieft <maarten.reg.github@kieft.ws>
2016-12-07 08:48:44 +0100

    move the test file to the processor, and outlined the steps

---
+++ src/processor/processor.py
@@ -0,0 +1,7 @@
+print ("Hello world!")
+
+#connect to serial
+#get a message back and put it into a model object
+#determine the right parser
+#parse the message and return a measurement object
+#store the measurement object in the database
