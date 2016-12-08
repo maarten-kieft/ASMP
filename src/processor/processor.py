a36b0ce653446c524fbe99a6a552f1d4d5e13c07
Maarten Kieft <maarten.reg.github@kieft.ws>
2016-12-08 08:13:47 +0100

    Implementing the processor

--- src/processor/processor.py
+++ src/processor/processor.py
@@ -1,5 +1,59 @@
-print ("Hello world!")
+import serial
+from time import sleep
 
+class Processor:
+    running = True 
+    
+    def start(self):
+        connection = self.createConnection()
+        message = []
+        
+        try:
+            connection.open()
+        except:
+            sys.exit ("Failed to open serial connection:"  % ser.name)
+
+        while self.running
+            line = ser.readline()
+            
+            if len(data) > 0:
+                message.apend(line)
+            elif len(message) > 0
+                self.parse(message)
+                message = []
+            else
+                sleep(1)
+        
+        print("Clossing connection")
+        connection.close()
+
+    def createConnection(self):
+        connection = serial.Serial()
+        connection.baudrate = 9600
+        connection.bytesize=serial.SEVENBITS
+        connection.parity=serial.PARITY_EVEN
+        connection.stopbits=serial.STOPBITS_ONE
+        connection.xonxoff=0
+        connection.rtscts=0
+        connection.timeout=02
+        connection.port="/dev/ttyUSB0"
+
+        return connection
+        
+
+    def stop(self):
+        self.running = False
+        print("Stopping..")
+
+    def parse(self, message)
+        print("Parssing message")
+        for i, line in enumerate(message):
+            print(i, line)
+    
+processor = Processor()
+
+processor.start()
+#processor.stop()
 #connect to serial
 #get a message back and put it into a model object
 #determine the right parser
