27e45057fd17ad76c7fa2bf8f56db34865d72a76
Maarten Kieft <maarten.reg.github@kieft.ws>
2016-12-22 22:46:41 +0100

    Reworked the processor, which is now a django app and uses the django orm

--- src/worker/parser.py
+++ src/worker/parser.py
@@ -43,7 +43,6 @@ class Parser:
             "1-0:1.8.2" : "usage_total_normal",
             "1-0:2.8.1" : "return_total_low",
             "1-0:2.8.2" : "return_total_normal",
-            "0-0:96.14.0" : "current_tarrif",
             "1-0:1.7.0" : "usage_current",
             "1-0:2.7.0" : "return_current"
         }
--- src/worker/processor.py
+++ src/worker/processor.py
@@ -1,80 +1,60 @@
-import serial
-from time import sleep
+"""Processing, intterpreting and storing serial messages"""
+from datetime import datetime
+import pytz
 from worker.parser import Parser
 from worker.connector import Connector
 from web.models import Meter, MeterMeasurement
 
 class Processor:
-    running = True 
+    """"Class responsible for listening for serial messages, interpreting and storing them"""
+    running = True
     parser = Parser()
     connector = Connector()
 
     def start(self):
+        """Starting the processor to listen for message, interpret and store them"""
         connection = self.connector.createConnection()
 
+        print("Connecting")
+        connection.open()
 
+        print("Listening")
+        self.listen(connection)
 
-        message = [
-            "/KFM5KAIFA-METER",
-            "1-3:0.2.8(42(4)",
-            "0-0:1.0.0(161211191412W)",
-            "0-0:96.1.1(4530303237303030358130313334353142)",
-            "1-0:1.8.1(000060.140*kWh)",
-            "1-0:1.8.2(000076.832*kWh)",
-            "1-0:2.8.1(000000.000*kWh)",
-            "1-0:2.8.2(000000.000*kWh)",
-            "0-0:96.14.0(0001)",
-            "1-0:1.7.0(00.577*kW)",
-            "1-0:2.7.0(00.000*kW)",
-            "0-0:96.7.21(00001)",
-            "0-0:96.7.9(00001)",
-            "1-0:99.97.0(1)(0-0:96.7.19)(000101000001W)(2147483647*s)",
-            "1-0:32.32.0(00000)",
-            "1-0:32.36.0(00000)",
-            "0-0:96.13.1()",
-            "0-0:96.13.0()",
-            "1-0:31.7.0(002*A)",
-            "1-0:21.7.0(00.580*kW)",
-            "1-0:22.7.0(00.000*kW)"
-        ]
-
-        parsed_message = self.parser.parse_message(message)
-
-        measurement = MeterMeasurement(**parsed_message)
-        #measurement.meter = Meter.objects.get_or_create(name=parsed_message["meter_name"])[0]
-
-        measurement.save()
-
-        print("Done!")
-        return
-
-        #print("Connecting")
-        #connection.open()
-
-        #print("Listening")
-        #self.listen(connection)
-
-        #print("Clossing connection")
-        #connection.close()
-
+        print("Clossing connection")
+        connection.close()
 
     def listen(self, connection):
+        """Listen for serial messages, interpret them and store them"""
         message = []
-       
+
         while self.running:
             line = str(connection.readline().decode("utf-8")).strip()
 
             if len(line) == 0:
                 continue
-            
+
             if line[0] == "!":
-                result = self.interpreter.interpret_message(message)
-               
-                result.measurement.save()
+                parsed_message = self.parser.parse_message(message)
+                measurement = self.interpret_message(parsed_message)
+                measurement.save()
+
                 message = []
             else:
                 message.append(line)
 
+    def interpret_message(self, parsed_message):
+        """Interpret the parsed message"""
+        meter = Meter.objects.get_or_create(name=parsed_message["meter_name"])[0]
+
+        del parsed_message["meter_name"]
+        measurement = MeterMeasurement(**parsed_message)
+        measurement.meter = meter
+        measurement.timestamp = datetime.now(pytz.utc)
+
+        return measurement
+
     def stop(self):
+        """Stopping the processor"""
         self.running = False
         print("Stopping..")
