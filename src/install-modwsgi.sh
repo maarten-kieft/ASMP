4fb3c80ef990d56637e1cc3a610325f39aed11dd
Maarten Kieft <maarten.reg.github@kieft.ws>
2017-01-09 09:00:03 +0100

    Working on docker

--- src/install-modwsgi.sh
+++ src/install-modwsgi.sh
@@ -1,2 +1,6 @@
-wget -o mod_wsgi_src.tar.gz https://github.com/GrahamDumpleton/mod_wsgi/archive/4.5.13.tar.gz 
-tar xf mod_wsgi_src.tar.gz
+wget -o /usr/bin/asmp/mod_wsgi_src.tar.gz https://github.com/GrahamDumpleton/mod_wsgi/archive/4.5.13.tar.gz 
+tar xf /usr/bin/asmp/mod_wsgi_src.tar.gz
+cd /usr/bin/asmp/mod_wsgi/
+./configure
+make
+make install
