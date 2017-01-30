837dba8c0b942482d8b5ff83cff8753fecefb7ce
Maarten Kieft <maarten.reg.github@kieft.ws>
2017-01-30 08:46:08 +0100

    Working on the docker volume issue

--- src/web/views.py
+++ src/web/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from web.models import MeterMeasurement, Meter
+from datetime import datetime
+import pytz
 
 def dashboard(request):
+    measurements = MeterMeasurement.objects.order_by('-timestamp')[:1]
+
+    return render(request, "dashboard.html", {'model' : measurements[0].timestamp.strftime("%Y-%m-%d %H:%M:%S")})
+
+def dashboard2(request):
     measurements = MeterMeasurement.objects.order_by('-timestamp')[:1]
 
 
@@ -18,7 +25,7 @@ def dashboard(request):
     measurement.return_current = 2.34
     measurement.return_total_low = 2.33
     measurement.return_total_normal = 3.33
-
+    measurement.timestamp = datetime.now(pytz.utc)
     measurement.save()
 
-    return render(request, "dashboard.html", {'model' : str(measurements[0].usage_current)})
+    return render(request, "dashboard.html", {'model' : "bla"})
