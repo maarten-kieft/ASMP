368421ffbd1d2efe22e15cc435042e67ee161598
Maarten Kieft <maarten.reg.github@kieft.ws>
2017-04-17 21:57:24 +0200

    Working on the statistics

--- src/web/models.py
+++ src/web/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
 class Meter(models.Model):
+    """The meter object"""
     name = models.CharField(max_length=80)
 
     class Meta:
+        """Meta data class"""
         db_table = "Meter"
 
 
 class Measurement(models.Model):
-    meter = models.ForeignKey(Meter,on_delete=models.CASCADE)
+    """THe readings from the meter"""
+    meter = models.ForeignKey(Meter, on_delete=models.CASCADE)
     timestamp = models.DateTimeField()
     usage_current = models.DecimalField(max_digits=10, decimal_places=3)
     usage_total_low = models.DecimalField(max_digits=10, decimal_places=3)
@@ -16,7 +19,20 @@ class Measurement(models.Model):
     return_current = models.DecimalField(max_digits=10, decimal_places=3)
     return_total_low = models.DecimalField(max_digits=10, decimal_places=3)
     return_total_normal = models.DecimalField(max_digits=10, decimal_places=3)
-    aggregated = models.BooleanField(default='0')
-    
+    aggregated = models.BooleanField(default="0")
+
     class Meta:
+        """Meta data class"""
         db_table = "Measurement"
+
+class Statistic(models.Model):
+    """Statistic based on the measurements"""
+    meter = models.ForeignKey(Meter, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField()
+    usage_total = models.DecimalField(max_digits=10, decimal_places=3)
+    return_total = models.DecimalField(max_digits=10, decimal_places=3)
+    estimated = models.BooleanField()
+
+    class Meta:
+        """Meta data class"""
+        db_table = "Statistic"
--- src/worker/aggregator.py
+++ src/worker/aggregator.py
@@ -1,10 +1,85 @@
 class Aggregator:
     """"Class responsible for aggregating the received messages"""
 
+    aggregateQuery = """
+	INSERT INTO (
+		meter_id
+		, timestamp
+		, usage_current
+		, usage_total_low
+		, usage_total_normal
+		, return_current
+		, return_total_low
+		, return_total_normal
+		, aggregated
+	)
+	SELECT 
+		meter_id
+		, timestamp
+		, avg(usage_current)
+		, max(usage_total_low)
+		, max(usage_total_normal) 
+		, avg(return_current)
+		, max(return_total_low)
+		, max(return_total_normal)
+		, 1
+	FROM (
+		select 
+			strftime('%Y-%m-%dT%H:00:00.000', timestamp) as timestamp
+			, usage_current
+			, usage_total_low
+			, usage_total_normal
+			, return_current
+			, return_total_low
+			, return_total_normal
+			, meter_id
+		FROM 
+			web_metermeasurement
+		WHERE	
+			aggregated = 0
+	) as data
+	GROUP BY 
+		meter_id
+		, timestamp
+	"""
 
+    cleanupQuery = """
 
+	"""
+
+    def run(self):
+        with connection.cursor() as cursor:
+            cursor.execute(aggregateQuery)
+
+    
 #1. je doet een groepering, 
 #2. Je groeppeert op uur, pakt max van al die velden en avg van huidig.
 #3. Je voegt die toe en verwijdert de source regels.
 # dat doe je herhaal je tot er niks te doen is, daarna sleep je een uur
 # di tmoet waars op een andere thread draaien
+
+"""
+
+
+
+SELECT 
+	  meter_id
+	, timestamp
+	, max(usage_total_low) + max(usage_total_normal) usage_total
+	, max(return_total_low) + max(return_total_normal) return_total
+FROM (
+	select 
+		  strftime('%Y-%m-%dT%H:00:00.000', timestamp) as timestamp
+		, usage_total_low
+		, usage_total_normal
+		, return_total_low
+		, return_total_normal
+		, meter_id
+	FROM 
+		web_metermeasurement
+) as data
+GROUP BY 
+	  meter_id
+	, timestamp
+
+"""
