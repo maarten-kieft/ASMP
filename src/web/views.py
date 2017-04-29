6258be76c755f801aea87ad1a280b905916466fd
Maarten Kieft <maarten.reg.github@kieft.ws>
2017-04-29 21:27:44 +0200

    Working on the statistics, implemented for day

--- src/web/views.py
+++ src/web/views.py
@@ -26,4 +26,42 @@ def get_last_current_usage(request):
 
 def get_statistics(request):
     """Return statistics based on the period"""
-    Stati
+    statistics = Statistic.objects.raw("""
+        SELECT 
+              min(id) as id
+            , meter_id
+            , timestamp as timestamp_start
+            , DATETIME(timestamp,'+1 days') as timestamp_end
+            , min(usage_start) as usage_start 
+            , max(usage_end) as usage_end
+            , min(return_start) as return_start
+            , max(return_end) as return_end
+        FROM (
+            select 
+                  id
+                , strftime('%Y-%m-%dT00:00:00.000', timestamp_start) as timestamp
+                , usage_start
+                , usage_end
+                , return_start
+                , return_end
+                , meter_id
+            FROM 
+                statistic
+        ) as data
+        WHERE
+            timestamp in (
+                strftime('%Y-%m-%dT00:00:00.000', datetime('NOW', '-60 days'))
+                , strftime('%Y-%m-%dT00:00:00.000', datetime('NOW','-61 day'))
+            ) 
+        GROUP BY 
+            meter_id
+            , timestamp
+        """)
+
+    model = [
+        {'timestamp' : statistics[0].timestamp_start, 'usage':statistics[0].usage_end - statistics[0].usage_start},
+        {'timestamp' : statistics[1].timestamp_start, 'usage':statistics[1].usage_end - statistics[1].usage_start}
+    ]
+
+    return JsonResponse(model,safe=False)
+
