c895a4941f9c5c200ab55139d60597d50a93f9aa
Maarten Kieft <maarten.acc.github@kieft.ws>
2017-08-15 21:43:14 +0200

    Working on the updater, finished the web component

--- src/updater/dockercomponent.py
+++ src/updater/dockercomponent.py
@@ -1,33 +1,44 @@
 import docker
+from core.services.messageservice import MessageService
 
 class DockerComponent:
+    """A docker component which run individually"""
     client = docker.from_env()
     container = None
 
-    def __init__(self, image_name,command, startup_parameters):
+    def __init__(self, image_name, **startup_parameters):
         self.image_name = image_name
-        self.command = command
         self.startup_parameters = startup_parameters
 
     def init(self):
+        """Initializes the component by pulling newest version stopping old containers and starting a new one"""
         self.pull()
         self.stop()
         self.start()
         self.cleanup()
 
     def pull(self):
+        """Pulling a new version of the image"""
+        MessageService.log_info("updater", "Pulling latest image: " + self.image_name)
         self.client.images.pull(self.image_name,"latest")
 
     def start(self):
-        self.container = self.client.containers.run(self.image_name,self.command,self.startup_parameters)
+        """Starting a new container for the given image"""
+        MessageService.log_info("updater", "Starting new container: " + self.image_name)
+        self.container = self.client.containers.run(self.image_name,None,detach=True, **self.startup_parameters)
         
     def stop(self):
-        for container in this.client.containers.list(status=running)
-            if image = container.image.tags[0] == self.image_name:
+        """Stopping al running containers for the given image"""
+        MessageService.log_info("updater", "Stopping running container: " + self.image_name)
+        
+        for container in self.client.containers.list(filters={"status":"running"}):
+            if self.image_name in container.image.tags[0]:
                 container.stop()
 
     def cleanup(self):
-        for image in this.client.images.list():
-            if image.tags[0] == self.image_name:
-                image.remove()
-    
+        """Removing old stopped containers for the given image"""
+        MessageService.log_info("updater", "Cleaning up: " + self.image_name)
+        
+        for container in self.client.containers.list(filters={"status":"exited"}):
+            if len(container.image.tags) > 0 and self.image_name in container.image.tags[0]:
+                container.remove()
--- src/updater/updater.py
+++ src/updater/updater.py
@@ -1,25 +1,26 @@
 import time
 from core.services.messageservice import MessageService
-from dockercomponent import DockerComponent
+from updater.dockercomponent import DockerComponent
 
 class Updater:
     """Class responsible for updating the whole docker container"""
     running = True
 
     def __init__(self):
-        command = "start.sh"
-        self.updater = DockerComponent("blackhawkdesign/asmp-updater-x64", command, [])
-        self.processor = DockerComponent("blackhawkdesign/asmp-processor-x64", command, [])
-        self.aggregator = DockerComponent("blackhawkdesign/asmp-aggregator-x64", command, [])
-        self.web = DockerComponent("blackhawkdesign/asmp-web-x64", command, [])
+        self.updater = DockerComponent("blackhawkdesign/asmp-updater-lin64")
+        self.processor = DockerComponent("blackhawkdesign/asmp-processor-lin64")
+        self.aggregator = DockerComponent("blackhawkdesign/asmp-aggregator-lin64")
+        self.web = DockerComponent("blackhawkdesign/asmp-web-lin64", ports={81:81})
 
     def start(self):
         """Stats the updater"""
-        self.processor.init()
-        self.aggregator.init()
+        #self.processor.init()
+        #self.aggregator.init()
         self.web.init()
+        MessageService.log_info("updater","Initialized components")
 
         while self.running:
+            MessageService.log_info("updater","Sleeping for 60 minutes for the next update check")
             time.sleep(60 * 60)
 
             if self.requires_update():
