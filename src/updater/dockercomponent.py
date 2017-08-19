cc89227d0db87e618a13852f26dce0a2b8a42805
Maarten Kieft <maarten.acc.github@kieft.ws>
2017-08-19 21:52:13 +0200

    Implemented to restart updater and stop previous versions

--- src/updater/dockercomponent.py
+++ src/updater/dockercomponent.py
@@ -4,11 +4,17 @@ from core.services.messageservice import MessageService
 class DockerComponent:
     """A docker component which run individually"""
     client = docker.from_env()
+    container_id = None
     container = None
 
-    def __init__(self, image_name, **startup_parameters):
+    def __init__(self, image_name, container_id = None, **startup_parameters):
         self.image_name = image_name
+        self.container_id = container_id
         self.startup_parameters = startup_parameters
+
+        if self.container_id is not None:
+            self.container = self.client.containers.get(container_id)
+        
 
     def init(self):
         """Initializes the component by pulling newest version stopping old containers and starting a new one"""
@@ -26,13 +32,14 @@ class DockerComponent:
         """Starting a new container for the given image"""
         MessageService.log_info("updater", "Starting new container: " + self.image_name)
         self.container = self.client.containers.run(self.image_name,None,detach=True, **self.startup_parameters)
-        
+        self.container_id = self.container.id
+
     def stop(self):
         """Stopping al running containers for the given image"""
         MessageService.log_info("updater", "Stopping running container: " + self.image_name)
         
         for container in self.client.containers.list(filters={"status":"running"}):
-            if len(container.image.tags) > 0 and self.image_name in container.image.tags[0]:
+            if len(container.image.tags) > 0 and self.image_name in container.image.tags[0] and (self.container_id is None or container.id != self.container_id):
                 container.stop()
 
     def cleanup(self):
--- src/updater/updater.py
+++ src/updater/updater.py
@@ -10,11 +10,7 @@ class Updater:
     
     running = False
     data_volume = None
-
-    def __init__(self): 
-        self.client = docker.from_env()
-        self.updater_container = self.client.containers.get(os.environ['HOSTNAME'])
-           
+   
     def start(self):
         """Stats the updater"""
         if not self.valid_update_container():
@@ -22,25 +18,29 @@ class Updater:
             return
 
         self.init_components()
-        self.running = True
-        
-        while self.running:
+
+        while True:
             MessageService.log_info("updater","Sleeping for 60 minutes for the next update check")
             time.sleep(60 * 60)
 
             if self.requires_update():
-                self.running = False
+                self.updater.pull()
+                self.updater.start()
+                return
 
     def valid_update_container(self):
         """Validates if this containers is started with the right parameters"""
         return True
 
     def init_components(self):
-        #self.updater = DockerComponent("blackhawkdesign/asmp-updater-lin64",volumes_from=[self.updater_container.id])
-        self.web = DockerComponent("blackhawkdesign/asmp-web-lin64", ports={81:81},volumes_from=[self.updater_container.id])
-        self.processor = DockerComponent("blackhawkdesign/asmp-processor-lin64",volumes_from=[self.updater_container.id])
-        self.aggregator = DockerComponent("blackhawkdesign/asmp-aggregator-lin64",volumes_from=[self.updater_container.id])
+        updater_container_id = os.environ['HOSTNAME']
+        self.updater = DockerComponent(image_name = "blackhawkdesign/asmp-updater-lin64", container_id = updater_container_id)
+        self.web = DockerComponent(image_name = "blackhawkdesign/asmp-web-lin64", ports={81:81},volumes_from=[self.updater_container.id])
+        self.processor = DockerComponent(image_name = "blackhawkdesign/asmp-processor-lin64",volumes_from=[self.updater_container.id])
+        self.aggregator = DockerComponent(image_name = "blackhawkdesign/asmp-aggregator-lin64",volumes_from=[self.updater_container.id])
         
+        self.updater.stop()
+        self.updater.cleanup()
         self.web.init()
         self.processor.init()
         self.aggregator.init()
