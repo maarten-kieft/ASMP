ca36971b13bf2fef2181e67dd53253a179136267
Maarten Kieft <maarten.acc.github@kieft.ws>
2017-10-16 08:45:25 +0200

    Adding more integration tests

--- src/test/integration/core/services/teststatisticservice.py
+++ src/test/integration/core/services/teststatisticservice.py
@@ -1,11 +1,33 @@
+from datetime import datetime, timedelta
 from django.test import TestCase
 from core.services.statisticservice import StatisticService
-from core.models import Statistic
+from core.models import Statistic, Meter
+import pytz
 
 class StatisticServiceTestCase(TestCase):
     def setUp(self):
         Statistic.objects.all().delete()
+        Meter.objects.all().delete()
 
-    def test_get_aggregated_statistics(self):
-        statistics = StatisticService.get_filtered_aggregated_statistics("hour",None)
-        self.assertEqual(0,len(statistics))
+    def test_create_statistics(self):
+        meter = Meter.objects.get_or_create(name="integrationtest-meter")[0]
+
+        aggregated_measurements = [
+            {
+                "meter_id" : meter.id,
+                "timestamp_start" :datetime.now(pytz.utc).replace(tzinfo=pytz.utc),
+                "usage_start" : 500,
+                "usage_end" : 1000,
+                "return_start" : 400,
+                "return_end": 800
+            }
+        ]
+
+        StatisticService.create_statistics(aggregated_measurements)
+        statistics = Statistic.objects.all()
+        self.assertEqual(1,len(statistics))
+        self.assertEqual(500,statistics[0].usage_start)
+        self.assertEqual(1000,statistics[0].usage_end)
+        self.assertEqual(400,statistics[0].return_start)
+        self.assertEqual(800,statistics[0].return_end)
+
