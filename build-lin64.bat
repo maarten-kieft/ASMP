2a850943a75452ea6fae0193978fdf8e927cf634
Maarten Kieft <maarten.acc.github@kieft.ws>
2017-10-20 21:29:57 +0200

    Using docker labels instead of tags to determine details about the image

--- build-lin64.bat
+++ build-lin64.bat
@@ -1,15 +1,9 @@
 @echo off
-set /p version="Enter version: "
 
-docker build -t blackhawkdesign/asmp-web-lin64:latest -t blackhawkdesign/asmp-web-lin64:%version% -f docker/lin64/Dockerfile-web-lin64 .
-docker build -t blackhawkdesign/asmp-processor-lin64:latest -t blackhawkdesign/asmp-processor-lin64:%version% -f docker/lin64/Dockerfile-processor-lin64 .
-docker build -t blackhawkdesign/asmp-aggregator-lin64:latest -t blackhawkdesign/asmp-aggregator-lin64:%version% -f docker/lin64/Dockerfile-aggregator-lin64 .
-docker build -t blackhawkdesign/asmp-updater-lin64:latest -t blackhawkdesign/asmp-updater-lin64:%version% -f docker/lin64/Dockerfile-updater-lin64 .
-
-docker push blackhawkdesign/asmp-web-lin64:%version%
-docker push blackhawkdesign/asmp-processor-lin64:%version%
-docker push blackhawkdesign/asmp-aggregator-lin64:%version%
-docker push blackhawkdesign/asmp-updater-lin64:%version%
+docker build -t blackhawkdesign/asmp-web-lin64:latest -f docker/lin64/Dockerfile-web-lin64 .
+docker build -t blackhawkdesign/asmp-processor-lin64:latest -f docker/lin64/Dockerfile-processor-lin64 .
+docker build -t blackhawkdesign/asmp-aggregator-lin64:latest -f docker/lin64/Dockerfile-aggregator-lin64 .
+docker build -t blackhawkdesign/asmp-updater-lin64:latest -f docker/lin64/Dockerfile-updater-lin64 .
 
 docker push blackhawkdesign/asmp-web-lin64:latest
 docker push blackhawkdesign/asmp-processor-lin64:latest
--- build-linarm.bat
+++ build-linarm.bat
@@ -1,15 +1,9 @@
 @echo off
-set /p version="Enter version: "
 
-docker build -t blackhawkdesign/asmp-web-linarm:latest -t blackhawkdesign/asmp-web-linarm:%version% -f docker/linarm/Dockerfile-web-linarm .
-docker build -t blackhawkdesign/asmp-processor-linarm:latest -t blackhawkdesign/asmp-processor-linarm:%version% -f docker/linarm/Dockerfile-processor-linarm .
-docker build -t blackhawkdesign/asmp-aggregator-linarm:latest -t blackhawkdesign/asmp-aggregator-linarm:%version% -f docker/linarm/Dockerfile-aggregator-linarm .
-docker build -t blackhawkdesign/asmp-updater-linarm:latest -t blackhawkdesign/asmp-updater-linarm:%version% -f docker/linarm/Dockerfile-updater-linarm .
-
-docker push blackhawkdesign/asmp-web-linarm:%version%
-docker push blackhawkdesign/asmp-processor-linarm:%version%
-docker push blackhawkdesign/asmp-aggregator-linarm:%version%
-docker push blackhawkdesign/asmp-updater-linarm:%version%
+docker build -t blackhawkdesign/asmp-web-linarm:latest -f docker/linarm/Dockerfile-web-linarm .
+docker build -t blackhawkdesign/asmp-processor-linarm:latest -f docker/linarm/Dockerfile-processor-linarm .
+docker build -t blackhawkdesign/asmp-aggregator-linarm:latest -f docker/linarm/Dockerfile-aggregator-linarm .
+docker build -t blackhawkdesign/asmp-updater-linarm:latest -f docker/linarm/Dockerfile-updater-linarm .
 
 docker push blackhawkdesign/asmp-web-linarm:latest
 docker push blackhawkdesign/asmp-processor-linarm:latest
