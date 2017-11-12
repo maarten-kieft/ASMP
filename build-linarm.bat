d6fc6120d2704a7836a31ba20a5766fc732e93c3
Maarten Kieft <maarten.acc.github@kieft.ws>
2017-11-12 20:42:27 +0100

    Updating build files

--- build-linarm.bat
+++ build-linarm.bat
@@ -1,17 +1,17 @@
 @echo off
 set /p version="Enter version: "
 
-docker build -t blackhawkdesign/asmp-web-linarm:latest -f docker/linarm/Dockerfile-web-linarm --build-arg VERSION=%version% .
-docker build -t blackhawkdesign/asmp-processor-linarm:latest -f docker/linarm/Dockerfile-processor-linarm --build-arg VERSION=%version% .
-docker build -t blackhawkdesign/asmp-aggregator-linarm:latest -f docker/linarm/Dockerfile-aggregator-linarm --build-arg VERSION=%version% .
-docker build -t blackhawkdesign/asmp-updater-linarm:latest -f docker/linarm/Dockerfile-updater-linarm --build-arg VERSION=%version% .
-
-docker push blackhawkdesign/asmp-web-linarm:%version%
-docker push blackhawkdesign/asmp-processor-linarm:%version%
-docker push blackhawkdesign/asmp-aggregator-linarm:%version%
-docker push blackhawkdesign/asmp-updater-linarm:%version%
+docker build -t blackhawkdesign/asmp-web-linarm:latest -t blackhawkdesign/asmp-web-linarm:%version% -f docker/linarm/Dockerfile-web-linarm --build-arg VERSION=%version% .
+docker build -t blackhawkdesign/asmp-processor-linarm:latest -t blackhawkdesign/asmp-web-linarm:%version% -f docker/linarm/Dockerfile-processor-linarm --build-arg VERSION=%version% .
+docker build -t blackhawkdesign/asmp-aggregator-linarm:latest -t blackhawkdesign/asmp-web-linarm:%version% -f docker/linarm/Dockerfile-aggregator-linarm --build-arg VERSION=%version% .
+docker build -t blackhawkdesign/asmp-updater-linarm:latest -t blackhawkdesign/asmp-web-linarm:%version% -f docker/linarm/Dockerfile-updater-linarm --build-arg VERSION=%version% .
 
 docker push blackhawkdesign/asmp-web-linarm:latest
 docker push blackhawkdesign/asmp-processor-linarm:latest
 docker push blackhawkdesign/asmp-aggregator-linarm:latest
 docker push blackhawkdesign/asmp-updater-linarm:latest
+
+docker push blackhawkdesign/asmp-web-linarm:%version%
+docker push blackhawkdesign/asmp-processor-linarm:%version%
+docker push blackhawkdesign/asmp-aggregator-linarm:%version%
+docker push blackhawkdesign/asmp-updater-linarm:%version%
