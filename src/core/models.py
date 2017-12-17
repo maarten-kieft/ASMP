2c158b22d9e7dad42c7ed54565bf9c623abffc64
Maarten Kieft <maarten.acc.github@kieft.ws>
2017-12-17 20:06:45 +0100

    Renaming fields of the measurement and Statistic models. To make room for gas usage

--- src/core/models.py
+++ src/core/models.py
@@ -13,12 +13,14 @@ class Measurement(models.Model):
     """THe readings from the meter"""
     meter = models.ForeignKey(Meter, on_delete=models.CASCADE, db_index=True)
     timestamp = models.DateTimeField(db_index=True)
-    usage_current = models.DecimalField(max_digits=10, decimal_places=3)
-    usage_total_low = models.DecimalField(max_digits=10, decimal_places=3)
-    usage_total_normal = models.DecimalField(max_digits=10, decimal_places=3)
-    return_current = models.DecimalField(max_digits=10, decimal_places=3)
-    return_total_low = models.DecimalField(max_digits=10, decimal_places=3)
-    return_total_normal = models.DecimalField(max_digits=10, decimal_places=3)
+    power_usage_current = models.DecimalField(max_digits=10, decimal_places=3)
+    power_usage_total_low = models.DecimalField(max_digits=10, decimal_places=3)
+    power_usage_total_normal = models.DecimalField(max_digits=10, decimal_places=3)
+    power_supply_current = models.DecimalField(max_digits=10, decimal_places=3)
+    power_supply_total_low = models.DecimalField(max_digits=10, decimal_places=3)
+    power_supply_total_normal = models.DecimalField(max_digits=10, decimal_places=3)
+    gas_usage_total = models.DecimalField(max_digits=10, decimal_places=3,default=0)
+    gas_using_now = models.BooleanField(default=0)
 
     class Meta:
         """Meta data class"""
@@ -29,10 +31,12 @@ class Statistic(models.Model):
     meter = models.ForeignKey(Meter, on_delete=models.CASCADE, db_index=True)
     timestamp_start = models.DateTimeField(db_index=True)
     timestamp_end = models.DateTimeField(db_index=True)
-    usage_start = models.DecimalField(max_digits=10, decimal_places=3)
-    usage_end = models.DecimalField(max_digits=10, decimal_places=3)
-    return_start = models.DecimalField(max_digits=10, decimal_places=3)
-    return_end = models.DecimalField(max_digits=10, decimal_places=3)
+    power_usage_start = models.DecimalField(max_digits=10, decimal_places=3)
+    power_usage_end = models.DecimalField(max_digits=10, decimal_places=3)
+    power_supply_start = models.DecimalField(max_digits=10, decimal_places=3)
+    power_supply_end = models.DecimalField(max_digits=10, decimal_places=3)
+    gas_usage_start = models.DecimalField(max_digits=10, decimal_places=3, default=0)
+    gas_usage_end = models.DecimalField(max_digits=10, decimal_places=3, default=0)
 
     class Meta:
         """Meta data class"""
