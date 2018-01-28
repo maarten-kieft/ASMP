619bc677d8adb19c6e7008c7ae007e5b2ff0ed62
Maarten Kieft <maarten.acc.github@kieft.ws>
2018-01-28 15:11:11 +0100

    Saving raw message for debugging purposses

--- src/web/views/log.py
+++ src/web/views/log.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from core.services.logservice import LogService
+from core.services.applicationservice import ApplicationService
 
 def index(request):
     """Returns the status screen"""
 
     recent_messages = LogService.get_recent()
     label_types = {'info' : 'info', 'warning' : 'warning', 'success' : 'success', 'error' : 'danger'}
-    model = {'messages' : recent_messages, 'label_types' : label_types}
+    model = {'messages' : recent_messages, 'label_types' : label_types, 'message_format' : ApplicationService.get_meter_message_format()}
 
     return render(request, "log.html", model)
